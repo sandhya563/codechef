e9efe682b47e6f0c0ed77fdef22ccb2ad697e56c
sandhya563 <60354285+sandhya563@users.noreply.github.com>
2020-12-19 09:51:15 +0530

    Add files via upload

---
+++ divide.py
@@ -0,0 +1,6 @@
+number1=int (input("enter any number"))
+number2=int(input("enter any number"))
+if (number2 != 0):
+    print (number1/number2)
+else:
+    print ("Number cannot be divided by zero") 
